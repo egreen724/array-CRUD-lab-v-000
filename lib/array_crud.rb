0275572e15b0696adc125e4eb2ee1d8ee0cb8fe0
Erin Green <erin.y.green@gmail.com>
2018-09-18 18:15:08 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [sweetwater, red hare, three taverns, monday night]
+  array = [sweetwater, red hare, three taverns, monday night]
 end
 
 def add_element_to_end_of_array(array, element)
